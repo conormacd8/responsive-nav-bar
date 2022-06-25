aa1d9c6aeba7069511555586110d9fcb8d6d0b80
Conor MacDonald <conormacd8@gmail.com>
2022-06-25 10:42:14 +1000

    Added js functionality

---
+++ index.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector('.nav-toggle')
+const nav = document.querySelector('.nav')
+
+navToggle.addEventListener('click', function(){
+  nav.classList.toggle('nav-open')
+})
